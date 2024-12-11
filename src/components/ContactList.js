9ccd84a75389207a951393d8054bd9102efdf1b4
IshaT-Hub <ishatiwari0508@gmail.com>
2024-12-12 00:22:29 +0530

    new

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,29 +2,37 @@ import React from "react";
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
-    console.log(props);
+  console.log(props);
 
-    const deleteContactHandler = (id) =>{
-        props.getContactID(id);
-    };
+  const deleteContactHandler = (id) => {
+    props.getContactID(id);
+  };
 
-    const contacts = [
-        {
-            id: 1,
-            name: "John",
-            email: "j@j.com"
-        }
-    ]
-    const renderContactList = contacts.map((contact) =>{
-        return(
-            <ContactCard contact = {contact} clickHandler = {deleteContactHandler} key = {contact.id} />
-        );
-    })
+  const contacts = [
+    {
+      id: 1,
+      name: "John",
+      email: "j@j.com",
+    },
+  ];
+  const renderContactList = contacts.map((contact) => {
     return (
-        <div className="ui celled list">
-            {renderContactList}
-        </div>
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      />
     );
-}
+  });
+  return (
+    <div className="main">
+      <div className="ui celled list">{renderContactList}</div>;
+      <h2>
+        Contact List
+        <button className="ui button blue right">Add Contacts </button>
+      </h2>
+    </div>
+  );
+};
 
 export default ContactList;
